85eabba59c06211a5ff6ed72bb82bad5c6fc23ef
Khang Tran <khangtrann8198@gmail.com>
2023-05-04 20:24:19 +0700

    Init menu container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuContainerComponent } from './components/menu-container/menu-container.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    HomepageComponent,
+    MenuContainerComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
---
+++ src/app/components/menu-container/menu-container.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-container',
+  templateUrl: './menu-container.component.html',
+  styleUrls: ['./menu-container.component.scss'],
+})
+export class MenuContainerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
